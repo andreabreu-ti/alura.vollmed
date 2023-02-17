87b5c3a58adc66ba41f646f95b631bee3954c5da
aandd <aandd@ABREU_DELL>
2023-02-17 10:28:04 -0300

    Spring Boot 3: 04.02 - Adicionando a lib auth0 jwt

--- alura.vollmed/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ alura.vollmed/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -25,6 +25,6 @@ public class AutenticacaoController {
 		var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
 		var autentication = manager.authenticate(token);
 		
-		return ResponseEntity.ok().build();
+		return ResponseEntity.ok("");
 	}
 }
