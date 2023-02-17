17a3039f1ce913561d526761c317a9dd89f0d680
aandd <aandd@ABREU_DELL>
2023-02-17 13:35:48 -0300

    Sprint Boot3: Parte 1 - Pacientes

---
+++ alura.vollmed/src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+	@PostMapping
+	public void cadastrarPaciente(@RequestBody String json) {
+		System.out.println(json);
+	}
+}
