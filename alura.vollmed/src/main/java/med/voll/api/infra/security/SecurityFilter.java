cec5d315dbaeaf3dd87575b448417b014ad3fbcf
aandd <aandd@ABREU_DELL>
2023-03-06 17:25:57 -0300

    Spring Boot 3: 05.04-Filter de Segurança OncePerRequestFilter

---
+++ alura.vollmed/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,28 @@
+package med.voll.api.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component //Componente genérico
+public class SecurityFilter extends OncePerRequestFilter{
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+
+		//Necessário para chamar os próximos filtros na aplicação
+		//Garantir que a linha seja executada
+		filterChain.doFilter(request, response);
+		
+	}
+
+	
+}
