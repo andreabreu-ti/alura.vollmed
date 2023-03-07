d14be432a9af883812ed4d250677ffbdfdcc8dda
andre_abreu <andre_abreu@04B-IN001324.agropalma.net>
2023-03-07 12:47:48 -0300

    Spring Boot 3: 05.05-Recuperando o token Authorization

--- alura.vollmed/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ alura.vollmed/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -10,19 +10,31 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component //Componente genérico
-public class SecurityFilter extends OncePerRequestFilter{
+@Component // Componente genérico
+public class SecurityFilter extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
 
-		//Necessário para chamar os próximos filtros na aplicação
-		//Garantir que a linha seja executada
-		filterChain.doFilter(request, response);
+		var tokenJWT = recuperarToken(request);
 		
+		System.out.println(tokenJWT);
+		
+		// Necessário para chamar os próximos filtros na aplicação,
+		// Garantir que a linha seja executada
+		filterChain.doFilter(request, response);
+
 	}
 
-	
+	private String recuperarToken(HttpServletRequest request) {
+
+		var authorizationHeader = request.getHeader("Authorization");
+		if (authorizationHeader == null) {
+			throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+		}
+
+		return authorizationHeader.replace("Bearer", "");
+	}
+
 }
