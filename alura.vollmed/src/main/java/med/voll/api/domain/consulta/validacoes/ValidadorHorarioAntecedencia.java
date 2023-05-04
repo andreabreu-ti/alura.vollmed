ab1006ee71a05754bf47877c5fec8aaa75c03195
andre_abreu <andre_abreu@04B-IN001324.agropalma.net>
2023-05-04 15:40:05 -0300

    Spring Boot 3: API Rest - Parte3 - 02.01 - Classes de validação

---
+++ alura.vollmed/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorHorarioAntecedencia {
+
+	public void validar(DadosAgendamentoConsulta dados) {
+		
+		var dataConsulta = dados.data();
+		var agora = LocalDateTime.now();
+		var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+		
+		if (diferencaEmMinutos < 30) {
+			
+			throw new ValidacaoException("Consulta deve ser agenda com antecedência mínima de 30 minutos");
+			
+		}
+		
+	}
+}
