2692c13c05a0eff71a72c88d792e18457bc69931
andre_abreu <andre_abreu@04B-IN001324.agropalma.net>
2023-05-05 15:37:00 -0300

    Spring Boot 3: API Rest - Parte3 - 03.03 - JWT documentação Bearer

--- alura.vollmed/src/main/java/med/voll/api/controller/ConsultaController.java
+++ alura.vollmed/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import med.voll.api.domain.usuario.AgendaDeConsultas;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 	
 	@Autowired
