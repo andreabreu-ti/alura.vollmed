9cc8cd148c34deb5245e15f1ff25986514dcd08d
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2026-01-16 16:51:45 -0300

    Configurações de segurança

--- alura.vollmed/src/main/java/br/com/alura/vollmed/infra/exception/TratadorDeErros.java
+++ alura.vollmed/src/main/java/br/com/alura/vollmed/infra/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package br.com.alura.vollmed.infra;
+package br.com.alura.vollmed.infra.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
