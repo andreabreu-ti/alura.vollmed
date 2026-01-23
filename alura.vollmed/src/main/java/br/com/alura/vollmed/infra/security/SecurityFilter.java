df0d30446e5277a2c849fb95de99a492caba3b71
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2026-01-23 10:51:14 -0300

    Recuperando o token

--- alura.vollmed/src/main/java/br/com/alura/vollmed/infra/security/SecurityFilter.java
+++ alura.vollmed/src/main/java/br/com/alura/vollmed/infra/security/SecurityFilter.java
@@ -16,13 +16,21 @@ public class SecurityFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
 		System.out.println("FILTRO CHAMADO!!!!");
+		var tokenJWT = recuperarToken(request);
+		System.out.println(tokenJWT);
 		
 		filterChain.doFilter(request, response); //Chamar os próximos filtros na aplicação
-		
-		
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+
+		var authorizationHeader = request.getHeader("Authorization");
+		if (authorizationHeader == null) {
+			throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+		}
+		return authorizationHeader.replace("Bearer ", ""); //Retirar o prefixo Bearer
 	}
 
 }
