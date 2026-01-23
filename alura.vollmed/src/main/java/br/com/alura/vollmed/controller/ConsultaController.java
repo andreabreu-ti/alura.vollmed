9700f2665cb684563d6e0ee314a8cef375cfba75
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2026-01-23 15:44:12 -0300

    Classe Service - Regras de Negócio

--- alura.vollmed/src/main/java/br/com/alura/vollmed/controller/ConsultaController.java
+++ alura.vollmed/src/main/java/br/com/alura/vollmed/controller/ConsultaController.java
@@ -1,11 +1,13 @@
 package br.com.alura.vollmed.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.vollmed.domain.consulta.AgendaDeConsultas;
 import br.com.alura.vollmed.domain.consulta.DadosAgendamentoConsulta;
 import br.com.alura.vollmed.domain.consulta.DadosDetalhamentoConsulta;
 import jakarta.transaction.Transactional;
@@ -15,11 +17,15 @@ import jakarta.validation.Valid;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+	@Autowired
+	private AgendaDeConsultas agenda;
+	
 	@PostMapping
 	@Transactional
 	public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
 		
 		System.out.println(dados);
+		agenda.agendar(dados);
 		return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
 	}
 }
--- alura.vollmed/src/main/java/br/com/alura/vollmed/domain/consulta/AgendaDeConsultas.java
+++ alura.vollmed/src/main/java/br/com/alura/vollmed/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,29 @@
 package br.com.alura.vollmed.domain.consulta;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.vollmed.domain.medico.MedicoRepository;
+import br.com.alura.vollmed.domain.paciente.PacienteRepository;
+import lombok.experimental.var;
+
+@Service
 public class AgendaDeConsultas {
 
+	@Autowired
+	private ConsultaRepository consultaRepository;
+	
+	@Autowired
+	private MedicoRepository medicoRepository;
+	
+	@Autowired
+	private PacienteRepository pacienteRepository;
+	
+	public void agendar(DadosAgendamentoConsulta dados) {
+		
+		var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+		var medico = medicoRepository.findById(dados.idMedico()).get();
+		var consulta = new Consulta(null, medico, paciente, dados.data());
+		consultaRepository.save(consulta);
+	}
 }
