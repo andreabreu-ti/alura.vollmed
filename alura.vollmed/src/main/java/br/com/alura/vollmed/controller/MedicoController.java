047db039c7ac250f048f31db105a54df121289ab
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2026-06-15 09:35:05 -0300

    Método de Listagem

--- alura.vollmed/src/main/java/br/com/alura/vollmed/controller/MedicoController.java
+++ alura.vollmed/src/main/java/br/com/alura/vollmed/controller/MedicoController.java
@@ -1,13 +1,17 @@
 package br.com.alura.vollmed.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.vollmed.medico.DadosCadastroMedico;
+import br.com.alura.vollmed.medico.DadosListagemMedico;
 import br.com.alura.vollmed.medico.Medico;
 import br.com.alura.vollmed.medico.MedicoRepository;
 import jakarta.validation.Valid;
@@ -24,5 +28,11 @@ public class MedicoController {
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 
 		repository.save(new Medico(dados));
+	}
+
+	@GetMapping
+	public List<DadosListagemMedico> listar(){
+		
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
 	}
 }
